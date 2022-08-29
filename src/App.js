2af18ff257efa45743994a1244e0b2842d49dd6d
SakibSaizeed <sakibsaizeed@gmail.com>
2022-08-29 13:12:37 +0600

    All installation and Folder Structure added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Home from "./Pages/HomePage/Home/Home";
+import About from "./Pages/HomePage/About/About";
+import Services from "./Pages/HomePage/Services/Services";
+import Login from "./Pages/Login/Login";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/Pages/HomePage/Services/Services.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+  return <div>this services</div>;
+};
+
+export default Services;
